ed0b9f4a6b5f8065a0cfd4a8845907e267a031ff
mteterel <merill.teterel@id6tm.org>
2022-09-26 16:47:38 +0200

    add kube cfg

--- packages/nightshift/src/app/run.command.ts
+++ packages/nightshift/src/app/run.command.ts
@@ -7,9 +7,11 @@ import { Command, CommandRunner } from 'nest-commander';
 export class TaskRunner extends CommandRunner {
   run(passedParams: string[], options?: Record<string, any>): Promise<void> {
     const random = Math.floor(Math.random() * 3);
+    const shouldCrash = random === 2;
+
     console.log("Hello from nest-commander !", { passedParams, options, SANDBOX_VAR: process.env.SANDBOX_VAR });
-    console.log(random === 2 ? "You are TERMINATED" : "Systems are functionning normally");
-    if (random === 2)
+    console.log(shouldCrash ? "You are TERMINATED" : "Systems are functionning normally");
+    if (shouldCrash)
       process.exit(1337);
 
     return;
